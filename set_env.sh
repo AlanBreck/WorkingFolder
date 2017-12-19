ca1e9a0bb6efa2d4ca59039789e23d64182aa3fa
Dean Radcliffe <deanmisc@yahoo.com>
2017-12-19 17:34:34 -0600

    Added set_env.sh

---
+++ set_env.sh
@@ -0,0 +1,17 @@
+# Begin WorkingFolder h4x
+# Edit this to your machine, if necessary,
+# updating ~.bash_profile if necessary
+export WF=~/Desktop/WorkingFolder
+
+# > echo `act pupster`
+function act {
+    find . -type d -iname \*$1\*
+}
+
+# > code `activity 1 9`
+function activity {
+    local mod=`printf %02d $1`
+    local act=`printf %02d $2`
+    ls -d -1 01-Class-Content/$mod*/01-Activities/$act*$3
+}
+# End WorkingFolder h4xx
